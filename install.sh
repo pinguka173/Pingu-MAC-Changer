8fef31409e9171321b973aab12ac079b0226b2f4
Pingu <pingugithub@proton.me>
2025-04-05 17:47:53 +0200

    Added update feature

--- install.sh
+++ install.sh
@@ -3,9 +3,11 @@
 SCRIPT_NAME="pingumac"  # the command name users will type
 TARGET_DIR="/usr/local/bin"
 SOURCE_SCRIPT="pingumac"       # your actual script filename
+INSTALL_SCRIPT="install.sh"
 
 # Get full path to the script
 SCRIPT_PATH="$(realpath "$SOURCE_SCRIPT")"
+INSTALL_SCRIPT_PATH="$(realpath "$INSTALL_SCRIPT")"
 
 # Check if the file exists
 if [ ! -f "$SCRIPT_PATH" ]; then
@@ -15,8 +17,10 @@ fi
 
 # Make sure it's executable
 chmod +x "$SCRIPT_PATH"
+chmod +x "$INSTALL_SCRIPT_PATH"
 
 # Create symlink
 sudo ln -sf "$SCRIPT_PATH" "$TARGET_DIR/$SCRIPT_NAME"
+sudo ln -sf "$INSTALL_SCRIPT_PATH" "$TARGET_DIR/install.sh"
 
 echo "$SCRIPT_NAME has been successfully installed!"
